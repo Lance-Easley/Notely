c7aba04e5f447160a568d6a9c44dc5f46f44b353
Lance Easley <atlasle52671015@gmail.com>
2021-06-07 15:04:24 -0500

    Initial Commit

---
+++ src/main/java/com/notely/app/NotelyApplication.java
@@ -0,0 +1,13 @@
+package com.notely.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notely/app/NotelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notely.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
